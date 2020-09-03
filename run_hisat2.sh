6dcb1e1617f140f5c2a09588232b92d83dbdfaa5
HuangLab-Auditory <70582238+HuangLab-Auditory@users.noreply.github.com>
2020-09-03 12:14:42 +0930

    Update run_hisat2.sh

--- run_hisat2.sh
+++ run_hisat2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #The pipeline is to download data from Li et.al 2018 with sratoolkits
-#Then align the fastq files into BAM files with hisat2 
+#Then align the fastq files into BAM files with hisat2
 #The BAM files are input for IGV
 
 #Directories
@@ -19,32 +19,37 @@ do
 
 #Dowloading data
 
-fastq-dump \
---outdir ${RAW} \
---split-files SRR67984$i
-  
+  fastq-dump \
+  --outdir ${RAW} \
+  --split-files SRR67984$i
+
 #Converting fastq files into SAM files
 
-echo aligning SRR67984$i into SAM...
-hisat2 -p 8 --dta -x ${INDEX} -1 ${RAW}/SRR67984${i}_1.fastq -2 ${RAW}/SRR67984${i}_2.fastq -S ${ALIGNED}/SRR67984$i.sam
+  echo aligning SRR67984$i into SAM...
+  hisat2 -p 8 \
+  --dta \
+  -x ${INDEX} \
+  -1 ${RAW}/SRR67984${i}_1.fastq \
+  -2 ${RAW}/SRR67984${i}_2.fastq \
+  -S ${ALIGNED}/SRR67984$i.sam
 
 #Sorting SAM files into BAM files and index the BAM files
 
-echo converting SAM into BAM...
+  echo converting SAM into BAM...
 
-samtools sort -@ 8 \
--o ${ALIGNED}/SRR67984$i.bam \
-${ALIGNED}/SRR67984$i.sam
+  samtools sort -@ 8 \
+  -o ${ALIGNED}/SRR67984$i.bam \
+  ${ALIGNED}/SRR67984$i.sam
 
 #Indexing
 
-samtools index ${ALIGNED}/SRR67984$i.bam
+  samtools index ${ALIGNED}/SRR67984$i.bam
 
 #Delete useless files
 
-rm ${RAW}/SRR67984${i}_1.fastq
-rm ${RAW}/SRR67984${i}_2.fastq
-rm ${ALIGNED}/SRR67984$i.sam
+  rm ${RAW}/SRR67984${i}_1.fastq
+  rm ${RAW}/SRR67984${i}_2.fastq
+  rm ${ALIGNED}/SRR67984$i.sam
 
 ((i=i+2))
 done
